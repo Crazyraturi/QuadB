4caca9bd8990373d1a3996019e67cfba9aac6d17
crazyraturi <subodhblogger966@gmail.com>
2025-05-05 15:07:10 +0530

    ....

--- week2_figma_assignment/jadoo/vite.config.js
+++ week2_figma_assignment/jadoo/vite.config.js
@@ -4,11 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"./",
   plugins: [react(),  tailwindcss(),],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom','lucide-react']
-    }
-  }
+  
 })
